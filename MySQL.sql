5f5943cf763c77fc33f5b802c05105d367c7b988
Miguel Aldana <maaldanac@gmail.com>
2025-01-19 21:44:44 -0500

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,2 +1,5 @@
 -- This file indicates that this project uses MySQL.
 -- The queries are integrated into the Jupyter notebook.
+
+-- Example query
+SELECT COUNT(*) AS total_crimes FROM Chicago_Crime;
